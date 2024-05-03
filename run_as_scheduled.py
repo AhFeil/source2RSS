5d7ed8a2c829214ab5443b72ca5351dcd398ac5c
ahfei <vgamebox@outlook.com>
2024-05-03 11:25:15 +0800

    run every hour

--- run_as_scheduled.py
+++ run_as_scheduled.py
@@ -13,7 +13,8 @@ def sync_wrapper():
 job = sync_wrapper
 if config.is_production:
     point = config.run_everyday_at
-    schedule.every().day.at(point).do(job)
+    # schedule.every().day.at(point).do(job)
+    schedule.every().hour.do(job)
 else:
     schedule.every(config.run_test_every_seconds).seconds.do(job)
 
