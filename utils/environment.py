1c93b57222b4500fbd060e4b1e10879e749209aa
ahfei <vgamebox@outlook.com>
2024-05-10 22:20:52 +0800

    user agent is stable now

--- utils/environment.py
+++ utils/environment.py
@@ -1,4 +1,5 @@
 import random
+import hashlib
 from typing import Dict, List, Optional, Tuple
 
 from playwright.async_api import Cookie, Page
@@ -6,8 +7,14 @@ from playwright.async_api import Cookie, Page
 from preprocess import config
 
 
-def get_user_agent() -> str:
-    return random.choice(config.desktop_user_agent)
+def get_user_agent(name4seed: str) -> str:
+    """根据字符串计算索引，返回 user agent，比如用账号名，这样每次返回的是同一个，合理"""
+    amount = len(config.desktop_user_agent)
+    hash_object = hashlib.md5(name4seed.encode('utf-8'))
+    hex_dig = hash_object.hexdigest()
+    # 使用取余运算将整数映射
+    index = int(hex_dig, 16) % amount
+    return config.desktop_user_agent[index]
 
 
 def get_mobile_user_agent() -> str:
@@ -23,3 +30,8 @@ def convert_cookies(cookies: Optional[List[Cookie]]) -> Tuple[str, Dict]:
     cookies_str = ";".join(f"{cookie.get('name')}={cookie.get('value')}" for cookie in cookies)
     cookie_dict = {cookie.get('name'): cookie.get('value') for cookie in cookies}
     return cookies_str, cookie_dict
+
+
+if __name__ == "__main__":
+    ans = get_user_agent("AhFei")
+    print(ans)
