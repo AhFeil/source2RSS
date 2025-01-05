1c5028b057bc05b9eed88d678499423814c62aa8
ahfei <vgamebox@outlook.com>
2025-01-05 23:44:44 +0800

    improve newsletter

--- website_scraper/newsletter.py
+++ website_scraper/newsletter.py
@@ -1,4 +1,5 @@
 import asyncio
+from datetime import datetime
 from typing import AsyncGenerator, Any
 
 from .example import WebsiteScraper
@@ -7,37 +8,38 @@ from utils.imap_client import ImapMailBox
 
 class NewsLetter(WebsiteScraper):
     title = "NewsLetter"
-    home_url = "http://example.com"
     page_turning_duration = 5
     key4sort = "pub_time"
 
-    def __init__(self, from_email: str, newsletter_name: str, host, port, username, password, ssl=True) -> None:
+    def __init__(self, froms: dict[str, str], host, port, username, password, ssl=True) -> None:
         super().__init__()
-        self.from_email = from_email
-        self.newsletter_name = newsletter_name
+        self.from_email = froms.get("from_email", "")
+        self.website = froms.get("website", "http://example.com")
+        self.newsletter_name = froms.get("name", "NameLack")
+        self.desc = froms.get("desc", "DescriptionLack")
         self.mailbox = ImapMailBox(host, port, username, password, ssl)
 
     @property
     def source_info(self):
         return {
             "title": f"NewsLetter of {self.newsletter_name}",
-            "link": self.__class__.home_url,
-            "description": f"NewsLetter of {self.newsletter_name}",
+            "link": self.from_email,
+            "description": self.desc or "DescriptionLack",   # 不能为空，否则无法生成 RSS
             "language": "En",
             "key4sort": self.__class__.key4sort}
 
     @classmethod
-    async def parse(cls, logger, from_email, mailbox: ImapMailBox) -> AsyncGenerator[dict, Any]:
+    async def parse(cls, logger, from_email, website, mailbox: ImapMailBox) -> AsyncGenerator[dict, Any]:
         logger.info(f"{cls.title} start to parse email {from_email}")
         with mailbox:
-            for mail in mailbox.get_mails():
+            for mail in mailbox.get_mails("ALL"):
                 # 当 from_email 不为空时，必须是 from_email 发来的邮件才会处理
                 if from_email and mail.from_addr != from_email:
                     continue
                 article = {
                     "article_name": mail.subject,
                     "summary": mail.plain,
-                    "article_url": "http://example.com",
+                    "article_url": website,
                     "image_link": "http://example.com",
                     "pub_time": mail.date.replace(tzinfo=None)   # 去除时区信息，否则无法对比
                 }
@@ -45,7 +47,7 @@ class NewsLetter(WebsiteScraper):
                 await asyncio.sleep(cls.page_turning_duration)
 
     def custom_parameter_of_parse(self) -> list:
-        return [self.from_email, self.mailbox]
+        return [self.from_email, self.website, self.mailbox]
 
 
 import api._v1
@@ -53,7 +55,8 @@ api._v1.register_c(NewsLetter)
 
 
 async def test():
-    w = NewsLetter("", "Test", host='imap.gmail.com', port=993, username="", password="")
+    froms = {"from_email": "", "website": "", "desc": "", "name": "Test"}
+    w = NewsLetter(froms, host='imap.gmail.com', port=993, username="", password="")
     print(w.source_info)
     print(w.table_name)
     async for a in w.first_add():
