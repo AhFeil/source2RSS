6df36b9037b5934ca82200945efcec9e0f3277d7
ahfei <vgamebox@outlook.com>
2025-07-12 17:26:04 +0800

    add user register page

--- src/web/user.py
+++ src/web/user.py
@@ -1,10 +1,11 @@
 """用户注册等接口"""
 import logging
 
-from fastapi import APIRouter
-from fastapi.responses import JSONResponse
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse, JSONResponse
 from pydantic import BaseModel, constr
 
+from .get_rss import templates
 from .security import UserRegistry
 
 logger = logging.getLogger(__name__)
@@ -22,6 +23,14 @@ class UserCreate(BaseModel):
     name: constr(pattern=username_password_regex, min_length=3, max_length=20) # type: ignore
     passwd: constr(pattern=username_password_regex, min_length=8, max_length=32) # type: ignore
 
+
+@router.get("/me/", response_class=HTMLResponse)
+async def user_page(request: Request):
+    return templates.TemplateResponse(request=request, name="user.html")
+
+@router.get("/me/register/", response_class=HTMLResponse)
+async def register_page(request: Request):
+    return templates.TemplateResponse(request=request, name="register.html")
 
 @router.post("/me/register", response_class=JSONResponse)
 async def user_register(user_data: UserCreate):
---
+++ tests/web/test_user.py
@@ -0,0 +1,45 @@
+"""对 Web 接口 user 测试"""
+import pytest
+from fastapi.testclient import TestClient
+
+from main import app
+
+client = TestClient(app)
+
+
+@pytest.fixture()
+def setup_and_tear_down():
+    print("This is run before each web test")
+    yield
+    print("This is run after each web test")
+
+
+def test_index_of_user(setup_and_tear_down):
+    response = client.get("/users/me/")
+    assert response.status_code == 200
+
+"""
+curl -X 'POST' \
+  'http://rss.vfly2.com/users/me/register' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "invite_code": "source2RSS",
+  "name": "pytest",
+  "passwd": "zfZZFgf56zsd56"
+}'
+"""
+def test_register(setup_and_tear_down):
+    data = {
+        "invite_code": "source2RSS",
+        "name": "pytest",
+        "passwd": "zfZZFgf56zsd56"
+    }
+    header = {
+        "accept": "application/json",
+        "Content-Type": "application/json"
+    }
+    response = client.post("/users/me/register", json=data, headers=header)
+    assert response.status_code == 200
+    response = client.post("/users/me/register", json=data, headers=header)
+    assert response.status_code == 200 # todo 重复注册会失败
